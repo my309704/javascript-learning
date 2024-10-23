ee81ee286bd5d53fd0a39eda3a504a3d07659167
my309704 <my309704@gmail.com>
2024-10-23 23:43:43 +0530

    01-variable-assignment.js
    my variable assignment is JS file.

---
+++ 01-variable-assignment.js
@@ -0,0 +1,15 @@
+
+var city = "Mumbai";
+console.log("My city name is:", city);
+var bestFriend = "Anand Yadav";
+console.log("My best firend is:", bestFriend);
+var countryName = "India";
+console.log("My country name is:", countryName);
+var schoolName = "Shivam vidya mandir high school";
+console.log("My school name is:", schoolName);
+var collegeName = "Chandrabhan sharma college";
+console.log("My college name is:", collegeName);
+
+
+
+
