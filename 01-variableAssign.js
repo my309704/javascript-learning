8eac417857f56da8c210211fb5098a13cced6f8e
my309704 <my309704@gmail.com>
2024-10-23 23:25:35 +0530

    my variableAssign.js file
    my variableAssign.js file

---
+++ 01-variableAssign.js
@@ -0,0 +1,42 @@
+// variable Decleration
+var myFullName
+console.log("myFullName:", myFullName); // undefined
+
+var myFullName = "Manoj Yadav"; //initialization
+console.log("my full name is: ", myFullName); // manoj yadav
+var myFullName = "Manoj Jayram Yadv";// variable update
+console.log("my full name is:", myFullName);// Manoj Jayram Yadav
+// Declare and initialization variable on same stage
+
+var myLove = "I love only JS";
+console.log("My love is:", myLove); // I love only JS
+
+// DECLARE AND INITIALIZATION VARIABLE ON THE SAME LINE
+var firstName = "Manoj";
+console.log("my first name is:", firstName);
+var lastName = "Yadav";
+console.log("My last name:", lastName);
+var age = "25";
+console.log("My age is:", age);
+var address = "Chandivali sakinaka mhada hiranandani andheri east Mumbai";
+console.log("My address is:", address);
+var pinCode = "400072";
+console.log("My address pin is: ", pinCode);
+var gender = "Male";
+console.log("My gender is:", gender);
+var isMarried = "True"
+console.log("isMarried:", isMarried);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 1-variable.js
@@ -0,0 +1,26 @@
+
+// variable decleration
+var city;
+console.log("city: ", city);  //undefined  
+// variable decleration
+var village;
+console.log("variable village value is: ", village); //undefined
+
+city = "Pune";  //initialization
+console.log("city value: ", city); //PUNE
+city = "Mumbai"; //VARIABLE UPDATE
+console.log("city value: ", city); //Mumbai
+
+city = "Delhi"; // Variable update
+console.log("city value: ", city); //Delhi
+
+//varialbe decleration and  initialization at same line
+var fullName ="Jenny Gates"
+console.log("fullName: ", fullName);
+
+
+//varialbe decleration and  initialization at same line
+var pin = "400072";
+console.log("pin: ", pin);
+var pin= "400107"; //variable update
+console.log("pin: ", pin);
