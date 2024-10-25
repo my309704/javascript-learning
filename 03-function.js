558fcaf73f13b07cfe7a25054cd9f76c0520d48f
my309704 <my309704@gmail.com>
2024-10-25 23:17:31 +0530

    FUNCTION WITH CALL AND INVOCATION
    FUNCTION WITH CALL AND INVOCATION

---
+++ 03-function.js
@@ -0,0 +1,41 @@
+
+// 
+// function decleration
+function show() {
+    console.log("Inside showfunction..");
+    console.log("===============");
+
+}
+//  function call or invocation
+show();
+show();
+
+// type 2: function with  argument and no return type  
+function square(num) {
+    console.log("given number to square:", num);
+    var result = num * num;
+    console.log("square is:", result);
+    console.log("=======================");
+
+
+}
+square(2);
+square(5);
+square(19);
+square(190890);
+
+// type 3: function with argument return value
+function multiply(n1, n2, n3){
+    console.log("given number:", n1, n2 ,n3);
+    var result = n1 * n2 * n3;
+    return result;
+    }
+var returnvalue = multiply(10, 35, 78);
+console.log("multiplication is:", returnvalue);
+console.log("================");
+
+var resultvalue = multiply(50, 70, 80);
+console.log("multiplication is:", resultvalue);
+
+
+
